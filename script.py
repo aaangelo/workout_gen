f06353c9a66eb7b8e4ab332efc59bb5785d2609a
aaangelo <100152932+aaangelo@users.noreply.github.com>
2022-02-21 17:41:48 +0000

    added class
    added a class as a little test, takes name and level and return both

--- script.py
+++ script.py
@@ -4,3 +4,19 @@ if test == 'yes':
     print('3 x 10 curls')
 elif test == 'no':
     print('don 3 x 10 pushups')
+
+class Profile:
+    def __init__(self, name, level):
+        self.name = name
+        self.level = level
+
+    def __repr__(self):
+        return 'your name is {name} and you are {level} level.'.format(name=self.name, level=self.level)
+
+name = input('what is your name?').title()
+level = input('how strong are you?')
+
+
+you = Profile(name, level)
+
+print(you)
